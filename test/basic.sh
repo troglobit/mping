882a571b15da9e4383ae762d534a51045d3eccb2
Joachim Wiberg <troglobit@gmail.com>
2021-08-30 20:44:19 +0200

    .github: allow receiver to start properly before the sender
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/basic.sh
+++ test/basic.sh
@@ -14,6 +14,7 @@ ip -br a | awk '$0="  "$0'
 print "Phase 1: Verify too few pings ..."
 ../mping -r -c 2 -i lo &
 PID=$!
+sleep 1
 
 ../mping -s -c 3 -i lo -W 2
 rc=$?
@@ -25,11 +26,11 @@ echo
 print "Phase 2: Verify successful ping ..."
 ../mping -d -r -c 3 -i lo &
 PID=$!
+sleep 1
 
 ../mping -d -s -c 3 -i lo -W 10
 rc=$?
 
-sleep 1
 kill -9 $PID 2>/dev/null
 [ $rc -ne 0 ] && FAIL
 OK
